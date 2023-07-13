69c63529953fb2e9bf0dc959ac72bb5b6119d56e
Braun_Alex <lamanasakasa@gmail.com>
2023-07-13 17:35:58 +0300

    ECDSA has been fully implemented and tested

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Braun-Alex/ECDSA/pkg/ecdsa"
+	"github.com/Braun-Alex/ECDSA/pkg/keypair"
+	"github.com/Braun-Alex/elliptic-wrapper/pkg/ec"
+)
 
 func main() {
-	fmt.Print("AAA")
+	message := "ZK-STARK has big impact on StarkNet"
+	var answer string
+	privateKey, publicKey := keypair.GenerateKeypair()
+	r, s := ecdsa.Sign(message, privateKey)
+	isValidSignature := ecdsa.Verify(message, publicKey, r, s)
+	if isValidSignature {
+		answer = "yes"
+	} else {
+		answer = "no"
+	}
+	fmt.Print("*********** Keypair and message ***********\n")
+	fmt.Printf("Private key: %s\n", privateKey.Text(ec.HexEncoding))
+	fmt.Printf("Public key: %s\n", ec.ElCPointToString(publicKey))
+	fmt.Print("Message: \"", message, "\"\n")
+	fmt.Print("********* ECDSA *********\n")
+	fmt.Printf("Parameter R: %s\n", r.Text(ec.HexEncoding))
+	fmt.Printf("Parameter S: %s\n", r.Text(ec.HexEncoding))
+	fmt.Print("Signature is valid: ", answer)
 }
